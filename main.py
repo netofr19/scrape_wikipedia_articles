ff893e4730e537a03ef6f320d516ff40e472809e
Francisco Procópio <francisco.procopio@enacom.com.br>
2024-02-15 14:01:08 -0300

    creation of scrapewikiarticle function

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-# In progress...
+import requests
+from bs4 import BeautifulSoup
+import random
+
+def scrapeWikiArticle(url, depth=5):
+
+    if depth == 0:
+        return None
+
+    response = requests.get(url=url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    title = soup.find(id='firstHeading')
+    print(title.text)
+
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScrape = 0
+
+    for link in allLinks:
+        # We are only interested in other wiki articles
+        if link['href'].find("/wiki/") == -1:
+            continue
+
+        # Use this link to scrape
+        linkToScrape = link
+        break
+
+    scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'], depth-1)
+
+
+if __name__ == "__main__":
+    scrapeWikiArticle("https://en.wikipedia.org/wiki/Web_scraping", depth=6)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# How to execute Random Scrape on Wikipedia Articles with Python
+
+In this project I'm going to create a random web scraper in Python that will scrape Wikipedia pages.
+
+The scraper will go to a Wikipedia page, scrape the title, and follow a random link to the next Wikipedia page. I think it will be fun to see what random Wikipedia pages this scraper will visit!
+
+## Installed libraries:
+```python
+pip install requests
+pip install beautifulsoup4
+```
